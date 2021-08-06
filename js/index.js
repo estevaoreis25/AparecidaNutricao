d6ff70ee2a4b86d75c1b7969c9c79152517559e9
estevaoreis25 <estevaoreis1587@gmail.com>
2021-08-06 14:52:53 -0300

    separating js files from html and selecting table valures

---
+++ js/index.js
@@ -0,0 +1,16 @@
+var titulo = document.querySelector(".titulo");
+titulo.textContent = "Nutricionais Aparecida :)";
+
+var paciente = document.querySelector("#primeiro-paciente");
+var tdPeso = paciente.querySelector(".info-peso");
+var peso = tdPeso.textContent;
+
+var tdAltura = paciente.querySelector(".info-altura");
+var altura = tdAltura.textContent;
+
+console.log(altura);
+console.log(peso);
+
+//var imc = peso/altura*altura;
+
+//console.log(imc);
