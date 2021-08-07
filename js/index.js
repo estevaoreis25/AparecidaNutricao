292db8213d4ab76a1583c8cffe22cf0f1507e06b
estevaoreis25 <estevaoreis1587@gmail.com>
2021-08-07 06:29:22 -0300

    Adding a new paciente on table

--- js/index.js
+++ js/index.js
@@ -27,8 +27,37 @@ pacientes.forEach(paciente => {
   }
 });
 
-var addPaciente = documente.querySelector("#adicionar-paciente");
+var addPaciente = document.querySelector("#adicionar-paciente");
 
 addPaciente.addEventListener("click", (event)=>{
   event.preventDefault();
+  var form = document.querySelector("#form-adiciona");
+
+  var nome = form.nome.value;
+  var peso = form.peso.value;
+  var altura = form.altura.value;
+  var gordura = form.gordura.value;
+
+  var pacienteTr = document.createElement("tr");
+
+  var nomeTd = document.createElement("td");
+  var pesoTd = document.createElement("td");
+  var alturaTd = document.createElement("td");
+  var gorduraTd = document.createElement("td");
+  var imcTd = document.createElement("td");
+
+  nomeTd.textContent = nome;
+  pesoTd.textContent = peso;
+  alturaTd.textContent = altura;
+  gorduraTd.textContent = gordura;
+
+  pacienteTr.appendChild(nomeTd);
+  pacienteTr.appendChild(pesoTd);
+  pacienteTr.appendChild(alturaTd);
+  pacienteTr.appendChild(gorduraTd);
+
+  var tabela = document.querySelector("#tabela-pacientes");
+  //console.log(tabela);
+  tabela.appendChild(pacienteTr);
+
 })
