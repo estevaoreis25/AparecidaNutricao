deb2c6cfbba09a47b7277b939521c43c1d940b68
estevaoreis25 <estevaoreis1587@gmail.com>
2021-08-14 14:05:52 -0300

    starting filter function

---
+++ js/filtra.js
@@ -0,0 +1,21 @@
+var campoFiltro = document.querySelector("#filtrar-tabela");
+campoFiltro.addEventListener("input", function(){
+  var pacientes = document.querySelectorAll(".paciente");
+
+
+  if(this.value.length > 0){
+    pacientes.forEach(paciente => {
+      var tdNome = paciente.querySelector(".info-nome");
+      var nomePaciente = tdNome.textContent;
+      if( nomePaciente != this.value) {
+        paciente.classList.add("invisivel");
+      } else {
+        paciente.classList.remove("invisivel");
+      }
+    });
+  }else {
+    pacientes.forEach(paciente => {
+        paciente.classList.remove("invisivel");
+    });
+  }
+});
